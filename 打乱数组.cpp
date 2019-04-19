c87ddcf9362eae15b11314e9b99ae03cbb650513
unknown <2332447236@qq.com>
2019-04-19 22:34:45 +0800

    like the title

---
+++ 打乱数组.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+    cout<<"Input the number of integers: ";
+    int num;
+    cin>>num;
+
+    int demo[num];
+    cout<<"Input your numbers: ";
+    for(int m=0;m<num;m++){
+        cin>>demo[m];
+    }
+
+    srand(time(0));
+    int j=rand()%6;   //Ëæ»úÊı
+
+    int temp;
+    for(int i=0;i<6;i++){
+        temp=demo[i];
+        demo[i]=demo[j];
+        demo[j]=temp;
+    }
+
+    cout<<"The scrambled array is: ";
+    for(int k=0;k<;k++){
+        cout<<demo[k]<<" ";
+    }
+    return 0;
+}
